f964e31e2fc9916355d672ef8a09746d59025d35
winstonjose01 <winston.jose@gmail.com>
2024-11-08 10:41:56 -0800

    update-6: fixin population formual and printing order

--- main.cpp
+++ main.cpp
@@ -9,13 +9,14 @@
 #include <array>
 #include <string>
 #include <sstream>
+#include<thread>
 using namespace std;
 
 const int DAYS_IN_MONTH[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-const string MONTH[] = {"Jan","Feb","march",
-                        "Apr","May","Jun",
-                        "Jul","Aug","Sep",
-                        "Oct","Nov","Dec"};
+const string MONTH[] = {"01","02","03",
+                        "04","05","06",
+                        "07","08","09",
+                        "10","11","12"};
 map<string,array<float,3>> load_environment_factors(string);
 
 int main(){
@@ -27,18 +28,18 @@ int main(){
     map<string, array<list<int>,3>> populationResults;
     map<string, array<float,3>> environment_factors;
     
-    Aphid aphid(1000);
-    Ant ant (600);
-    Ladybug ladybug(125);
+    Aphid aphid(8000);
+    Ant ant (1900);
+    Ladybug ladybug(1125);
 
+    int m = 0;
     for (string month : MONTH) {
-        int m = 0;
-        for (int i = 0; i <= DAYS_IN_MONTH[0]; i++){
-            string date = month + to_string(i);
-            //cout << date << endl;
+        for (int i = 1; i <= DAYS_IN_MONTH[m]; i++){
+            string date = month + "-" + to_string(i);
+            //cout << date << " - " << m << " - " << i << endl;
             environment_factors = load_environment_factors(filename);
 
-            cout << date<< "-" << environment_factors[date][0] <<  "," << environment_factors[date][1] << endl;
+            //cout << date<< "-" << environment_factors[date][0] <<  "," << environment_factors[date][1] << endl;
             
             if (environment_factors.find(date) != environment_factors.end()){
                 temperature = environment_factors[date][0];
@@ -54,6 +55,7 @@ int main(){
             aphid_pop =  aphid.get_initial_pop();
             ant_pop = ant.get_initial_pop();
             ladybug_pop = ladybug.get_initial_pop();
+            //cout << aphid_pop << " - " << ant_pop << " - " << ladybug_pop <<endl;
 
             aphid.set_current_pop(temperature,UV_index,precipitation,ladybug_pop,ant_pop);
             ant.set_current_pop(temperature,precipitation,aphid_pop);
@@ -66,14 +68,16 @@ int main(){
             aphid.set_initial_pop(aphid.get_current_pop());
             aphid.set_initial_pop(aphid.get_current_pop());
             aphid.set_initial_pop(aphid.get_current_pop());
+
         }
         m++;
     }
 
 
-    // for (const auto& [date,populations] : populationResults) {
-    //     cout << date << " - Aphid Population: " << populations[0].back() << ", Ant Population: " << populations[1].back() << ", Ladybug Population: " << populations[2].back() << std::endl;
-    // }
+    for (const auto& [date,populations] : populationResults) {
+        cout << date << " - Aphid Population: " << populations[0].back() << ", Ant Population: " << populations[1].back() << ", Ladybug Population: " << populations[2].back() << std::endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
 
 
     return 0;
@@ -94,7 +98,7 @@ map<string,array<float,3>> load_environment_factors(string filename){
         getline(stream, temperature,',');
         stream >> precipitation >> UV_index;
         //cout << month << day << "-" << temperature << endl;
-        string date = month + day;
+        string date = month + "-"+ day;
 
         environment_factors[date] = {stof(temperature),precipitation,UV_index};
         //cout << date<< "-" << environment_factors[date][0] <<  "," << environment_factors[date][1] << endl;
